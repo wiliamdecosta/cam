b06e1c0fe55786670e1e05eabffbe1df4137875d
saepudin06 <saepudin06@gmail.com>
2017-03-15 16:36:52 +0700

    ''

---
+++ application/libraries/product/Product_controller.php
@@ -0,0 +1,61 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Json library
+* @class Customer_controller
+* @version 07/05/2015 12:18:00
+*/
+class Product_controller {
+
+    function readLovProduct() {
+
+        $start = getVarClean('current','int',0);
+        $limit = getVarClean('rowCount','int',5);
+
+        $sort = getVarClean('sort','str','address_name');
+        $dir  = getVarClean('dir','str','asc');
+
+        $searchPhrase = getVarClean('searchPhrase', 'str', '');
+
+        $data = array('rows' => array(), 'success' => false, 'message' => '', 'current' => $start, 'rowCount' => $limit, 'total' => 0);
+
+        try {
+
+            $ci = & get_instance();
+            $ci->load->model('lov/customer');
+            $table = $ci->customer;
+            //$table = new Account($customer_ref); //kalau ada param
+
+            //Set default criteria. You can override this if you want
+            foreach ($table->fields as $key => $field){
+                if (!empty($$key)){ // <-- Perhatikan simbol $$
+                    if ($field['type'] == 'str'){
+                        $table->setCriteria($table->getAlias().$key.$table->likeOperator." '".$$key."' ");
+                    }else{
+                        $table->setCriteria($table->getAlias().$key." = ".$$key);
+                    }
+                }
+            }
+
+            if(!empty($searchPhrase)) {
+                $table->setCriteria("(upper(customer_ref) ".$table->likeOperator." upper('%".$searchPhrase."%') OR upper(first_name) ".$table->likeOperator." upper('%".$searchPhrase."%'))");
+            }
+
+            $start = ($start-1) * $limit;
+            $items = $table->getAll($start, $limit, $sort, $dir);
+            $totalcount = $table->countAll();
+
+            $data['rows'] = $items;
+            $data['success'] = true;
+            $data['total'] = $totalcount;
+
+        }catch (Exception $e) {
+            $data['message'] = $e->getMessage();
+        }
+
+        return $data;
+    }
+
+   
+}
+
+/* End of file Groups_controller.php */
